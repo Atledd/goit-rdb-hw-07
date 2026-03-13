9af9f85fdac4fd0ed2c8feef491a39f1f075c75f
Atledd <artem.gonzha@gmail.com>
2026-03-13 20:35:53 +0200

    SQL queries

---
+++ queries.sql
@@ -0,0 +1,36 @@
+SELECT 
+    id,
+    date,
+    YEAR(date) AS year,
+    MONTH(date) AS month,
+    DAY(date) AS day
+FROM orders;
+
+SELECT 
+    id,
+    date,
+    DATE_ADD(date, INTERVAL 1 DAY) AS next_day
+FROM orders;
+
+SELECT 
+    id,
+    date,
+    UNIX_TIMESTAMP(date) AS timestamp_value
+FROM orders;
+
+SELECT 
+    COUNT(*) AS order_count
+FROM orders
+WHERE date BETWEEN 
+'1996-07-10 00:00:00'
+AND 
+'1996-10-08 00:00:00';
+
+SELECT 
+    id,
+    date,
+    JSON_OBJECT(
+        'id', id,
+        'date', date
+    ) AS json_data
+FROM orders;
